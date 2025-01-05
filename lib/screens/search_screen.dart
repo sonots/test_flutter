7d2debe1d01a7dfc256e3aa1785ff54e520f3b5d
sonots <sonots@gmail.com>
2025-01-05 23:10:22 +0900

    widget

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,24 +1,65 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:http/http.dart' as http; // http$B$H$$$&JQ?t$rDL$7$F!"(Bhttp$B%Q%C%1!<%8$K%"%/%;%9(B
+import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:qiita_search/models/article.dart';
+import 'package:qiita_search/widgets/article_container.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({
+    super.key,
+  });
 
   @override
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<Article> articles = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Qiita Search'),
       ),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          // 検索ボックス
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 12,
+              horizontal: 36,
+            ),
+            child: TextField(
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+              decoration: const InputDecoration(
+                hintText: '検索ワードを入力してください',
+                hintStyle: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              onSubmitted: (value) async {
+                final result = await searchQiita(value);
+                setState(() => articles = result);
+              },
+            ),
+          ),
+          // 検索結果リストを追加
+          Expanded(
+            child: ListView(
+              children: articles
+                  .map((article) => ArticleContainer(article: article))
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -31,7 +72,7 @@ class _SearchScreenState extends State<SearchScreen> {
     final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
     final http.Response res = await http.get(uri, headers: {
       'Authorization': 'Bearer $token',
-     });
+    });
 
     if (res.statusCode == 200) {
       final List<dynamic> body = jsonDecode(res.body);
